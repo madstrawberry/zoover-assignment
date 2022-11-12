d5c945e2d8acd8f2816194ec0069595a5c667006
madstrawberry <a.engelse@gmail.com>
2022-11-12 22:16:21 +0100

    Add initial setup for accomodation page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,31 @@
-import './App.css';
+import "./App.css";
+import { PageTitle } from "./components/PageTitle";
+import { Subtitle } from "./components/Subtitle";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Here you go with Zoover</h1>
-      <em>
-        Note: This is a placeholder for the accommodation page that you need to
-        create, so don't keep it, and do all the changes you see important
-      </em>
-      <h2>Check the end points on server:</h2>
-      <ul>
-        <li>
-          <a href="http://localhost:8080/reviews">
-            http://localhost:8080/reviews
-          </a>
-        </li>
-        <li>
-          <a href="http://localhost:8080/reviews/average">
-            http://localhost:8080/reviews/average
-          </a>
-        </li>
-      </ul>
+    <div className="mt-8">
+      <div className="max-w-5xl px-8 mx-auto">
+        <PageTitle>XXX Accomodation</PageTitle>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+
+        <div className="mt-5 ">
+          <Subtitle>The average ratings for this accomodation</Subtitle>
+        </div>
+
+        <div className="mt-5 ">
+          <Subtitle>The percentages of travelledWith</Subtitle>
+        </div>
+      </div>
     </div>
   );
 }
