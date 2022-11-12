82f7d79a28b4040f73c919bedee9370afaa9fe97
madstrawberry <a.engelse@gmail.com>
2022-11-13 00:10:44 +0100

    Have inputs react to initial state for filters/sorting

--- client/src/components/accomodation/Reviews.tsx
+++ client/src/components/accomodation/Reviews.tsx
@@ -2,6 +2,12 @@ import { ChangeEvent, useEffect, useState } from "react";
 import { Review } from "../../../../server/interfaces";
 import { ReviewsApi } from "../../ReviewsApi";
 
+const filters = ["", "FAMILY", "FRIENDS", "SINGLE", "COUPLE", "OTHER"];
+const sorting = [
+  ["Review Date", "entryDate"],
+  ["Trip Date", "travelDate"],
+];
+
 export function Reviews() {
   const [isFetchingReviews, setIsFetchingReviews] = useState(true);
   const [reviews, setReviews] = useState<Review[]>([]);
@@ -9,7 +15,7 @@ export function Reviews() {
   const [filterState, setFilterState] =
     useState<ReviewsApi.ReviewFilterOptions>({
       filterBy: "",
-      sortBy: "",
+      sortBy: "entryDate",
       limit: 10,
       start: 1,
     });
@@ -32,6 +38,13 @@ export function Reviews() {
     }));
   };
 
+  const onSortBy = (e: ChangeEvent<HTMLInputElement>) => {
+    setFilterState((filterState) => ({
+      ...filterState,
+      sortBy: e.target.value,
+    }));
+  };
+
   useEffect(() => {
     fetchReviews(filterState);
   }, [filterState]);
@@ -40,19 +53,40 @@ export function Reviews() {
     <>
       <div className="bg-slate-100 border-y border-slate-200 mt-5">
         <div className="max-w-5xl px-8 py-3 mx-auto">
-          Filter reviews by:{" "}
-          <select
-            onChange={onSelectFilter}
-            name="travelledWith"
-            id="travelledWith"
-          >
-            <option value="">ALL</option>
-            <option value="FAMILY">FAMILY</option>
-            <option value="FRIENDS">FRIENDS</option>
-            <option value="SINGLE">SINGLE</option>
-            <option value="COUPLE">COUPLE</option>
-            <option value="OTHER">OTHER</option>
-          </select>
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              Filter reviews by:{" "}
+              <select
+                onChange={onSelectFilter}
+                name="travelledWith"
+                id="travelledWith"
+              >
+                {filters.map((filter) => (
+                  <option
+                    value={filter}
+                    selected={filterState.filterBy === filter}
+                  >
+                    {filter || "ALL"}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="ml-auto flex">
+              {sorting.map(([sortTitle, sortName]) => (
+                <div className="ml-4">
+                  <input
+                    onChange={onSortBy}
+                    type="radio"
+                    id={sortName}
+                    name="sortBy"
+                    value={sortName}
+                    checked={filterState.sortBy === sortName}
+                  />
+                  <label htmlFor={sortName}>{sortTitle}</label>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
 
